29f45028e5ff9aecab565c3568e992e776ee83d0
Ionnutvieru <79754041+Ionnutvieru@users.noreply.github.com>
2022-07-13 11:41:21 +0300

    Added Dark-Mode functionality

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal);
+
+// Light and Dark Mode
+
+var checkbox = document.getElementById("switch");
+var BodyCards = document.getElementsByClassName("card");
+var socialButtons = document.getElementsByClassName("fa-icon");
+
+checkbox.addEventListener("change", function() {
+  if (checkbox.checked) {
+    darkmode(); 
+  } else {
+    removedarkmode(); 
+  }
+})
+
+function darkmode() {
+  document.body.classList.add("dark-mode");
+  for(i = 0; i < BodyCards.length; ++i) {
+    BodyCards[i].classList.add("darkmodeCards");
+  }
+  for(i = 0; i < socialButtons.length; ++i) {
+    socialButtons[i].style.color = "white";
+  };
+  checkbox.checked = true;
+}
+
+
+function removedarkmode() {
+  document.body.classList.remove("dark-mode");
+  for(i = 0; i < BodyCards.length; ++i) {
+    BodyCards[i].className ='card';
+  }
+  for(i = 0; i < socialButtons.length; ++i) {
+    socialButtons[i].style.color = "black";
+  };
+  checkbox.checked = false;
+}
